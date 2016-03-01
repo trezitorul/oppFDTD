e3fa23c6ac22ae19aaef3d276ebee082dfbe146b
Samuel Trezitorul <trezitorul@gmail.com>
2016-02-29 23:19:29 -0500

    Initial commit. This code has not been tested yet, but it is fully complete. It is the basic engine for the FDTD engine.

---
+++ #operators.jl
@@ -0,0 +1,29 @@
+#operator.jl
+#This file contains the operator type definitions and base functions
+
+type operator
+#each concrete subtype of operator should implement the following methods
+#BC::Function, returns value of transform type to be used. 
+# These are BC for which Transform to be used in space and time 
+#Transforms::function applied using elements of inputted FDS
+#transform(i,j,k,t, FDS): performs in place update of FDS according to BC 
+#and transform 
+	BC::function #A boundary condition, can be either a matrix or function anything indexable. It can be used in different ways
+	#If Transforms contains operators then BC will contain indices of which operators should be applied at which timestep index 
+	#In addition to which spacial location
+	#If Transforms is simply a function that operatates directly on FDS then BC will hold data relevant to the transformation required such 
+	#as epsilon and mu.
+	Transforms#This contains the operations to be done on FDS
+	operator(Transforms, BC::function)=new(Transforms,BC)#Constructor for the operator. 
+
+function applyOperator!(i,j,k,t,opp::operator,FDS)#Applys the operators transforms, if Transforms contains functions then they are applied
+#However, operators will frequently contain other operators with their own BC IE refractive index...etc. 
+	applyOperator!(i,j,k,t,Transforms[BC(i,j,k,t)], FDS)#Applies the operator in Transforms based on what the BC conditions require.
+
+function applyOperator!(i,j,k,t,opp::function, FDS)#If we get to a base class of operator then the base class will 
+	opp(i,j,k,t,FDS, BC)#Apply the operator Transform to the FDS at index i,j,k,t
+	#Once we reach a base operator (IE an operator that has Transforms = a function that updates FDS) then it simply operates on the data
+	#This terminates the hierarchical recursive operator structure. 
+
+
+
---
+++ FDTD.jl
@@ -0,0 +1,35 @@
+#FDTD.jl
+#This is the file that will contain the main FDTD loop and initialization.
+#Additionally this file will contain the Field Data Structure (FDS) array.
+#FDS is structured as follows. (X Coord, Y Coord, Z Coord, t step*, FieldQuantityIndex**, FieldIndex)
+#*tstep, the tstep is a cycling coordinate, given the current time increment, everything above the current index is in the past
+#This way only part of FDS is rewritten everytime the code runs. Instead of rewriting the entire matrix.
+#**Field Quantity Index: E field component would be represented by 0 and H by 1 and Dipole orientation by 2...etc
+#Field is the dimension of the FieldQuantity being referenced. IE 0 might be Ex
+#FDS will also contain a time component which allows it to store historical data as 
+#Required by the algorithms implemented by the operators system.
+
+module FDTD 
+
+export simulate
+
+using operators
+#Simulate is the core of the FDTD engine, this is where the FDS structure (which holds all of the E and H fields) is updated for the next timestep.
+#steps is the number of timesteps to execute before terminating operation.
+#sampleRate is how often FDS should be written to disk for later animation
+#FDS is the Field Data Structure, it holds all of the information about the field
+#simulator, is the operator that updates the FDS for each timestep
+function simulate(steps::Int64, sampleRate::Int8, FDS::Array{Float64}, simulator::operator)
+	for t=1:steps#The integer time step. The way the algorithm increments including the time step size is set in the simulator operator
+		for i=0:size(FDS,1)#The x spacial coordinate
+			for j=0:size(FDS,2)#The y spacial coordinate
+				for k=0:size(FDS,3)#The z spacial coordinate
+					applyOperator!(i,j,k,t,simulator,FDS)#Applys the simulator operator to FDS, the operator type definition is found in operators.jl
+					#The simulator operator is found in a separate file that is included in a particular simulation run.
+					if mod(t,sampleRate)==0#Checks to see if it is time to save an FDS snapshot to disk
+						writeFDSToFile(FDS)#Saves FDS to disk
+					end
+
+#Writes the current frame of the FDS (IE one full frame of the FDS) to a database on disk
+function writeFDSToFile(FDS)
+	print("Not Implemented Yet")
